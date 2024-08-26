543c634d5105c2365fddbf98fea37ea60f4bdf02
hashimo000 <hashimon.hashimon@icloud.com>
2024-08-26 14:54:23 +0900

    ユーザー登録画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params[:user])
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
