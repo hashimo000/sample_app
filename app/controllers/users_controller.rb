76410f7f1a7f1d065e3640527200b541e8c7f1d6
hashimo000 <hashimon.hashimon@icloud.com>
2024-08-28 20:05:29 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params[:user])
+    @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # 保存の成功をここで扱う。
     else
       render 'new'
     end
   end
 
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
