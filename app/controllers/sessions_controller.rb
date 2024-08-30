aff665afad85d391101b2fa6ca2031a106e4eb81
hashimo000 <hashimon.hashimon@icloud.com>
2024-08-30 17:55:19 +0900

    ユーザーログインテスト作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # エラーメッセージを作成する
       render 'new'
     end
   end
